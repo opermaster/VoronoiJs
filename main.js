e8c713805c7bc6c905a003ea078a1307c93b24fb
opermaster <82831888+opermaster@users.noreply.github.com>
2023-12-21 14:21:54 +0200

    Source code uploaded

---
+++ main.js
@@ -0,0 +1,68 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+let colors=["green","red","blue","Aqua","Orange","Fuchsia","Teal","Chartreuse"]
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+function fill_blackscreen(height,width,color){
+    ctx.fillStyle=color;
+    for (let i=0;i<height;i++){
+        for (let j=0;j<width;j++){
+            ctx.fillRect(i,j,1,1);
+        }
+    }
+}
+
+function generate_seeds(height,width,count){
+    let seeds=new Object();
+    for (let i=0;i<count;i++){
+        let color=colors[getRandomInt(colors.length)];
+        let x=getRandomInt(height);
+        let y=getRandomInt(width);
+        seeds[color]=[x,y];
+        //console.log(color+" "+ x+" "+y);
+    }
+    return seeds;
+}
+function generate_voronoi(height,width,seeds,Euclid){
+    for (let i=0;i<height;i++){
+        for (let j=0;j<width;j++){
+            let distances=[];
+            for(let seed in seeds){
+                let x=seeds[seed][0];
+                let y=seeds[seed][1];
+                let seed_with_dist={
+                    color:seed,
+                    distance:Euclid?Math.sqrt(Math.pow(x-i,2)+Math.pow(y-j,2)):Math.abs(x-i)+Math.abs(y-j)
+                }
+                distances.push(seed_with_dist);
+            }
+                let nearest=distances.reduce(function (prev, current) {
+                return prev.distance < current.distance ? prev : current;});  
+                ctx.fillStyle=nearest.color;    
+                //console.log(nearest.color);       
+                ctx.fillRect(i,j,1,1);
+        }
+    }
+}
+function generate_dots(seeds){
+    for(let seed in seeds){
+        let x =seeds[seed][0];
+        let y =seeds[seed][1];
+        ctx.beginPath();
+        ctx.arc(x, y, 3, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "black";
+        ctx.fill();
+    }
+}
+const screen_width=800;
+const screen_height=800;
+fill_blackscreen(screen_width,screen_height);
+let seeds =generate_seeds(screen_width,screen_height,10);
+for(let seed in seeds){
+    console.log(seed+" "+seeds[seed][0]+" "+seeds[seed][1]);
+}
+
+generate_voronoi(screen_width,screen_height,seeds,true);//main function
+generate_dots(seeds);
